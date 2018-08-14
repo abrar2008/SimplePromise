dd6454e823835d5344bcc9c4a7736160b8311e30
mohammad ibrar <m.abrar2018@gmail.com>
2018-08-15 01:40:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+
+console.log('hello')
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }).catch(function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  }
+
+
+
+
+
+  function mom (){
+  var will = new Promise(
+    function (resolve, reject) {
+        if (isMomHappy) {
+            var phone = {
+                brand: 'Samsung',
+                color: 'black'
+            };
+            resolve(phone); // fulfilled
+        } else {
+            var reason = new Error('mom is not happy');
+            reject(reason); // reject
+        }
+
+    }
+);
+}
+
+mom.then(function(data)
+{
+  console.log('data')
+
+}
+.catch(function (error) {
+  console.log(error.message);
+}));
